0cc50056ecd36837fe86adbc609997d42e9e9077
ManishVerma0000 <Manishverma3224@gmail.com>
2024-04-11 17:31:25 +0530

    update profile is added

--- controllers/registerTenate.js
+++ controllers/registerTenate.js
@@ -2,7 +2,7 @@ const tenat = require('../schema/tenatModel')
 
 const registerTenate = async (req, res) => {
     try {
-        console.log(req.file, 'this is the value of the file')
+
         const { username, email, phone, address, orgnisation, buildingno, dateofjoining, rent } = req.body;
         console.log(req.body, 'this is the value of the body')
         const savedb = await tenat.create(req.body,
@@ -18,5 +18,81 @@ const registerTenate = async (req, res) => {
         await res.status(400).send(error.message)
     }
 }
+
+
+const updateTenate = async (req, res) => {
+    try {
+
+        const _id = req.params.id;
+        var userdetails;
+        if (!_id) {
+            await res.status(400).send({ message: "Please provide the id for the update" })
+        }
+        else {
+            userdetails = tenat.findById(_id);
+
+
+        }
+
+        const updateObject = {}
+        const { username, email, phone, address, orgnisation, buildingno, dateofjoining, rent } = req.body;
+        if (username) {
+            updateObject['username'] = username
+        } else {
+            updateObject['username'] = userdetails['username']
+        }
+
+        if (email) {
+            updateObject['email'] = email
+        }
+        else {
+            updateObject['email'] = userdetails['email']
+        }
+
+        if (phone) {
+            updateObject['phone'] = phone
+        }
+        else {
+            updateObject['phone'] = userdetails['phone']
+        }
+        if (address) {
+            updateObject['address'] = address
+        }
+        else {
+            updateObject['address'] = userdetails['address']
+        }
+
+        if (orgnisation) {
+            updateObject['orgnisation'] = orgnisation
+        }
+        else {
+            updateObject['orgnisation'] = userdetails['orgnisation']
+        }
+        if (buildingno) {
+            updateObject['buildingno'] = buildingno
+        } else {
+            updateObject['buildingno'] = userdetails['buildingno']
+        }
+        if (dateofjoining) {
+            updateObject['dateofjoining'] = dateofjoining
+        } else {
+            updateObject['dateofjoining'] = userdetails['dateofjoining']
+        }
+        if (rent) {
+            updateObject['rent'] = rent
+        } else {
+            updateObject['rent'] = userdetails['rent']
+        }
+
+        const updaateDb = await tenat.findByIdAndUpdate(_id, updateObject)
+        await res.status(200).send(updaateDb)
+
+    } catch (error) {
+
+        await res.status(400).send(error.message)
+    }
+}
+
+
 
 module.exports = registerTenate
