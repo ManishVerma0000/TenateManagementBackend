5ac2a926117ad3d4d61af9b7cb6783509af9187c
ManishVerma0000 <Manishverma3224@gmail.com>
2024-05-27 19:00:35 +0530

    changes in the db file

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.get('/', async (req, res) => {
 
 const ipAddress = '192.168.1.2'
 
-app.listen(port, ipAddress, () => {
+app.listen(port, () => {
     console.log(`server is listen on the port on  http://${ipAddress}:${port}`)
 })
 //172.19.224.1
