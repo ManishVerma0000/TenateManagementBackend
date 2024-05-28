39938a5d0b4f3321bdc94470099082227055faa9
ManishVerma0000 <Manishverma3224@gmail.com>
2024-05-28 13:40:53 +0530

    new comit

--- controllers/monthlycharge.js
+++ controllers/monthlycharge.js
@@ -1,11 +1,36 @@
+const Tenat = require("../schema/tenatModel");
+
 const monthlyCharge = async (req, res) => {
     try {
+        let dateObj = new Date();
+        dateObj.setMonth(dateObj.getMonth() + 1);
+        dateObj.setDate(1);
+        let year = dateObj.getFullYear();
+        let month = String(dateObj.getMonth() + 1).padStart(2, "0");
+        let day = String(dateObj.getDate()).padStart(2, "0");
+        let formattedDate = `${year}-${month}-${day}`;
 
+        const totalChargeMonthWise = await Tenat.aggregate([
+            {
+                $match: {
+                    NextInstallement: formattedDate
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalWaterCharge: { $sum: { $toInt: "$waterCharge" } },
+                    totalElectricityCharge: { $sum: { $toInt: "$electricitycharge" } },
+                    totalOtherCharge: { $sum: { $toInt: "$otherCharge" } },
+                    totalRent: { $sum: { $toInt: "$rent" } }
+                }
+            }
+        ]);
 
+        res.status(200).json(totalChargeMonthWise);
     } catch (error) {
-        await res.status(400).send({ message: error.message })
-
+        res.status(400).json({ message: error.message });
     }
-}
+};
 
-module.exports = monthlyCharge
+module.exports = monthlyCharge;
